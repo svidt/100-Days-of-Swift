2251f88158ab5edcefa9f4c7280d62a1a124510e
Emil <79602784+svidt@users.noreply.github.com>
2023-04-13 12:12:52 +0200

    Update 3.3 Compound Assignment Operators.swift

--- 1-12 Introduction to Swift/3.3 Compound Assignment Operators.swift
+++ 1-12 Introduction to Swift/3.3 Compound Assignment Operators.swift
@@ -1,18 +1,10 @@
-// Operators are the mathematical symbols like " + " and " - ", here's an example.
+// Swift has a shortcut for combining one operator with an assignment. They each look like " + , - , * , / " but with an " = " attached to the end them. In the example, *score* 95, *score* -= 5 ( subtracts 5 points and assign it back to the variable *score*. It works on Strings as well.
 
 import Foundation
 
-let firstScore = 12
-let secondScore = 4
+var score = 95
+score -= 5
 
-let total = firstScore + secondScore // addition
-let difference = firstScore - secondScore // subtraction
-
-let product = firstScore * secondScore // multiplication
-let divided = firstScore / secondScore // division
-
-
-// remainder after division. It calculates how many times a number can fit inside another, then returns the values of what's left. Foe example, we can set *secondScore* to **4**, and do *13 % secondScore*, we will get back **1**, because *4* fits into *13* three times with a remainder of *1*.
-
-let remainder = 13 % secondScore
+var quote = "The rain in Spain falls mainly on the "
+quote += "Spaniards"
 
