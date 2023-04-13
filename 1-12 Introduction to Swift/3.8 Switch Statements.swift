639888d50d71657d618eda427a5609b507ecd3ac
Emil <79602784+svidt@users.noreply.github.com>
2023-04-13 12:11:38 +0200

    adjustments

--- 1-12 Introduction to Swift/3.8 Switch Statements.swift
+++ 1-12 Introduction to Swift/3.8 Switch Statements.swift
@@ -1,4 +1,4 @@
-// Instead of having a long and complicated * if / else * setup, *Switch Case* offer a more clear overview. The last case - **default** triggers if none of the cases are true.  Swift only run the code inside the case, if you want the execution to contines: use the **fallthrough** keyword.
+// Instead of having a long and complicated *if / else* setup, *Switch Case* offer a more clear overview. The last case - **default** triggers if none of the cases are true.  Swift only run the code inside the case, if you want the execution to contines: use the **fallthrough** keyword.
 
 import Foundation
 
