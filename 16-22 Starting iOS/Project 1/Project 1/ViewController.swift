8552cced35dfb5e8dcf9175eba0ffbcb81bba5d0
Emil <79602784+svidt@users.noreply.github.com>
2023-04-26 13:08:01 +0200

    new files

---
+++ 16-22 Starting iOS/Project 1/Project 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Kristian Emil Hansen Svidt on 26/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
