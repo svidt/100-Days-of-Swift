06eb7274815f4daf6613954536688170ace3078f
Emil <79602784+svidt@users.noreply.github.com>
2023-04-26 13:04:56 +0200

    new files

---
+++ 16-22 Starting iOS/Project 1/Project 1/Project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1App.swift
+//  Project 1
+//
+//  Created by Kristian Emil Hansen Svidt on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
