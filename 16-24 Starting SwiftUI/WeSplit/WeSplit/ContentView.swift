bb9fb777899edd2fd4c14f9d3de85ac2b6dc804e
Emil <79602784+svidt@users.noreply.github.com>
2023-04-27 18:57:51 +0200

    SwiftUI Project 1

---
+++ 16-24 Starting SwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Svidt on 27/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Goodbye, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
