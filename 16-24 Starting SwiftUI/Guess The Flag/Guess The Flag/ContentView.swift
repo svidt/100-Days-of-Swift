570db854ba3dc50cab27a0b957ada3bec04410a1
Emil <79602784+svidt@users.noreply.github.com>
2023-04-30 12:18:58 +0200

    new files

--- 16-24 Starting SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
+++ 16-24 Starting SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: 0) {
+                LinearGradient(gradient: Gradient(colors: [.yellow, .white]), startPoint: .top, endPoint: .bottomLeading)
+                
+                //                AngularGradient(gradient: Gradient(colors: [.red, .orange, .yellow, .white]), center: .center)
+                
+                
+                //                RadialGradient(gradient: Gradient(colors: [.white, .orange]), center: .bottom, startRadius: 50, endRadius: 500)
+                
+                //                LinearGradient(gradient: Gradient(stops: [
+                //                    Gradient.Stop(color: .yellow, location: 0.75),
+                //                    Gradient.Stop(color: .orange, location: 1)
+                //                ]), startPoint: .top, endPoint: .bottom)
+                
+            }
+            
+            HStack {
+                
+                Text("Primary")
+                    .foregroundColor(.primary)
+                    .padding(25)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(25)
+                Text("Secondary")
+                    .foregroundStyle(.secondary)
+                    .padding()
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(25)
+                Button("Delete", role: .destructive) {
+                    print("deleted")
+                    showingAlert = true
+                }
+                .buttonStyle(.borderedProminent)
+                .alert("Important Message", isPresented: $showingAlert) {
+                    Button("Cancel", role: .cancel) { }
+                    Button("Delete", role: .destructive) { }
+                } message: {
+                    Text("Please read this.")
+                }
+                
+            }
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
