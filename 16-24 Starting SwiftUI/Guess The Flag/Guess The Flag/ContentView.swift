7563bb8a8c1a9c3500e025de2bf008bc63d9582e
Emil <79602784+svidt@users.noreply.github.com>
2023-05-02 12:04:34 +0200

    updated

--- 16-24 Starting SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
+++ 16-24 Starting SwiftUI/Guess The Flag/Guess The Flag/ContentView.swift
@@ -11,34 +11,36 @@ struct ContentView: View {
     
     @State private var showingScore: Bool = false
     @State private var scoreTitle: String = "Have a guess"
+    @State private var scoreSubtitle: String = ""
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var score = 0
+    @State private var totalTries = 3
     
     var body: some View {
         
         ZStack {
             
-// // Providede by AI
-//            RadialGradient(
-//                            gradient: Gradient(colors: [
-//                                Color(
-//                                    red: 64/255,
-//                                    green: 0/255,
-//                                    blue: 64/255),
-//                                Color(red: 200/255,
-//                                      green: 0/255,
-//                                      blue: 200/255
-//                                     )
-//                            ]),
-//                            center: .center,
-//                            startRadius: 0,
-//                            endRadius: UIScreen.main.bounds.width
-//                        )
-//                        .ignoresSafeArea()
-
+            // // Providede by AI
+            //            RadialGradient(
+            //                            gradient: Gradient(colors: [
+            //                                Color(
+            //                                    red: 64/255,
+            //                                    green: 0/255,
+            //                                    blue: 64/255),
+            //                                Color(red: 200/255,
+            //                                      green: 0/255,
+            //                                      blue: 200/255
+            //                                     )
+            //                            ]),
+            //                            center: .center,
+            //                            startRadius: 0,
+            //                            endRadius: UIScreen.main.bounds.width
+            //                        )
+            //                        .ignoresSafeArea()
+            
             
             LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topTrailing, endPoint: .bottomLeading).ignoresSafeArea()
             
@@ -63,7 +65,7 @@ struct ContentView: View {
                         } label: {
                             Image(countries[number])
                                 .renderingMode(.original)
-    //                            .clipShape(Capsule())
+                            //                            .clipShape(Capsule())
                                 .cornerRadius(20)
                                 .shadow(color: .white, radius: 1)
                         }
@@ -73,12 +75,13 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 50)
                 .background(.thinMaterial)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
+                .clipShape(RoundedRectangle(cornerRadius: 20))
                 
                 Spacer()
                 Spacer()
                 Text("Score: \(score)").font(.title3.bold()).foregroundStyle(.secondary)
-                Text(scoreTitle).font(.title2.bold()).foregroundStyle(.secondary)
+                Text("Chances: \(totalTries)").font(.title3.bold()).foregroundColor(.secondary)
+                Text(scoreTitle).font(.title2.bold()).foregroundStyle(.secondary).padding(10)
                 Spacer()
             }
             .padding(25)
@@ -87,17 +90,25 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is \(score)")
+            Text(scoreSubtitle)
         }
         
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
-            scoreTitle = "Correct"
             score += 1
+            scoreTitle = "Correct"
+            scoreSubtitle = "Your score is \(score)"
+            
+        } else if totalTries < 1 {
+            scoreTitle = "Too bad"
+            scoreSubtitle = "Better luck next time. Your score is \(score)"
+            resetAll()
         } else {
+            totalTries -= 1
             scoreTitle = "Wrong"
+            scoreSubtitle = "The correct answer was \(countries[correctAnswer])"
         }
         showingScore = true
     }
@@ -106,10 +117,15 @@ struct ContentView: View {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func resetAll() {
+        score = 0
+        totalTries = 3
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
