70229ac685aa5714427d510defc4e48817266848
Emil <79602784+svidt@users.noreply.github.com>
2023-05-04 13:44:09 +0200

    adjustments

--- 16-24 Starting SwiftUI/WeSplit/WeSplit/ContentView.swift
+++ 16-24 Starting SwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -7,26 +7,22 @@
 
 import SwiftUI
 
-
-
 struct ContentView: View {
     
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 0
+    @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-    @State private var customPercentage = 0.0
     
     @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
-        let tipSelection = Double(tipPercentage)
         
+        let tipSelection = Double(tipPercentage)
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
-        var amountPerPerson = grandTotal / peopleCount
+        let amountPerPerson = grandTotal / Double(numberOfPeople)
         
         return amountPerPerson
     }
@@ -39,18 +35,10 @@ struct ContentView: View {
         return totalCost
     }
     
-    var costCombined: Double {
-        
-        let customPercentage = Double(customPercentage)
+    var totalCost: Double {
         let tipSelection = Double(tipPercentage)
         let tipAdded = checkAmount / 100 * tipSelection
         let totalCost = tipAdded + checkAmount
-        
-        if customPercentage > 0 {
-            let newTipAdded = checkAmount / 100 * customPercentage
-            let newTotalCost = newTipAdded + checkAmount
-        }
-        
         
         return totalCost
     }
@@ -59,13 +47,12 @@ struct ContentView: View {
         
         NavigationView {
             Form {
-                
                 Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    TextField("Check Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocused)
                     Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2..<11) {
+                        ForEach(0..<9) {
                             Text("\($0) people")
                         }
                     }
@@ -73,37 +60,41 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    Picker("Tip percentage", selection: $tipPercentage) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
                         ForEach(tipPercentages, id: \.self) {
                             Text($0, format: .percent)
                         }
                     }
                     .pickerStyle(.segmented)
                     
-                    TextField("Other amount", value: $tipPercentage, format: .percent)
+                    TextField("Other Amount", value: $tipPercentage, format: .percent)
                 } header: {
-                    Text("Added tip")
+                    Text("Added Tip")
                 }
+                .keyboardType(.decimalPad)
+                .focused($amountIsFocused)
                 
                 Section {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 } header: {
-                    Text("Amount per person")
+                    Text("Amount Per Person")
                 } footer: {
                     VStack(alignment: .leading, spacing: 2) {
-                        Text("\(costTip, format: .currency(code: Locale.current.currency?.identifier ?? "USD")) Tip")
                         Text("\(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")) Check")
-                        Text("\(costCombined, format: .currency(code: Locale.current.currency?.identifier ?? "USD")) Total")
+                        Text("\(costTip, format: .currency(code: Locale.current.currency?.identifier ?? "USD")) Tip")
+                        Text("\(totalCost, format: .currency(code: Locale.current.currency?.identifier ?? "USD")) Total").bold()
                     }
                     .padding(.top, 10.0)
                     
                 }
             }
+            .onAppear {
+                
+            }
             .navigationTitle("WeSplit")
             .toolbar {
                 ToolbarItemGroup(placement: .keyboard) {
                     Spacer()
-                    
                     Button("Done") {
                         amountIsFocused = false
                     }
