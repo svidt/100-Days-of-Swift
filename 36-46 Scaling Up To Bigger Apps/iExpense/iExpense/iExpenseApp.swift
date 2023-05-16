6adeecadd89add56f0c92307ee91046bf710b4b4
Emil <79602784+svidt@users.noreply.github.com>
2023-05-16 16:17:50 +0200

    new project

---
+++ 36-46 Scaling Up To Bigger Apps/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Svidt on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
