582d6fd91df2395d532a5e0fc0a76437ce77408f
Emil <79602784+svidt@users.noreply.github.com>
2023-05-22 13:14:00 +0200

    New files added

---
+++ 36-46 Scaling Up To Bigger Apps/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Svidt on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 36-46 Scaling Up To Bigger Apps/Moonshot/README.md
@@ -0,0 +1,7 @@
+## Moonshot
+
+GeometryReader, ScrollView and NavigationLink
+<p align="center">
+<img src="Screenshot.png" width="350">
+
+</p>
