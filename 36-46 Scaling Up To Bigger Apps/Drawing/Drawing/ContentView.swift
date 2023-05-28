ae5443a9f77e46c01ca7c53ab542697615f0f601
Emil <79602784+svidt@users.noreply.github.com>
2023-05-28 22:53:30 +0100

    challenge

--- 36-46 Scaling Up To Bigger Apps/Drawing/Drawing/ContentView.swift
+++ 36-46 Scaling Up To Bigger Apps/Drawing/Drawing/ContentView.swift
@@ -7,14 +7,38 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            // Code
-        }
+struct Arrow: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY * 2))
+            
+        return path
     }
 }
 
+struct ContentView: View {
+    @State private var amount = 8.0
+    
+    var body: some View {
+        VStack {
+            Arrow()
+                .stroke(.red, style: StrokeStyle(lineWidth: amount, lineCap: .round, lineJoin: .round))
+                .frame(width: 200, height: 200)
+                .onTapGesture {
+                    withAnimation{
+                        amount += 10
+                        
+                        
+                    }
+                }
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
