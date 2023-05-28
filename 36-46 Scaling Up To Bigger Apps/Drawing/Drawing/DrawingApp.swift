b39ef9c55013609dda4e5b9b35eaafc8c784de01
Emil <79602784+svidt@users.noreply.github.com>
2023-05-28 11:52:51 +0100

    new files

---
+++ 36-46 Scaling Up To Bigger Apps/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Kristian Emil Hansen Svidt on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
