5808fe99e0e7375500701c46e775777d462651ea
Emil <79602784+svidt@users.noreply.github.com>
2023-05-28 18:00:00 +0100

    updates

--- 36-46 Scaling Up To Bigger Apps/Drawing/Drawing/ContentView.swift
+++ 36-46 Scaling Up To Bigger Apps/Drawing/Drawing/ContentView.swift
@@ -7,38 +7,10 @@
 
 import SwiftUI
 
-struct Arc: InsettableShape {
-    let startAngle: Angle
-    let endAngle: Angle
-    let clockwise: Bool
-    var insetAmount = 0.0
-    
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-        
-        var path = Path()
-        
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-        
-        return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
-    }
-}
-
 struct ContentView: View {
     var body: some View {
         VStack {
-            Arc(startAngle: .degrees(0), endAngle: .degrees(90), clockwise: true)
-                .strokeBorder(lineWidth: 100)
-            Circle()
-                .strokeBorder(lineWidth: 100)
+            // Code
         }
     }
 }
