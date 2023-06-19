4520e4ec3fea13324c7eb97ec26b3b8c8c9429b4
Emil <79602784+svidt@users.noreply.github.com>
2023-06-19 21:56:53 +0100

    Project 12

---
+++ 49-59 Focus On Data/CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Kristian Emil Hansen Svidt on 19/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    @StateObject private var dataController = DataController()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+}
