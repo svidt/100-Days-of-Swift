a00e7ff5f343043f8ab4bb8fac9d56944cdc22f3
Emil <79602784+svidt@users.noreply.github.com>
2023-06-26 21:46:43 +0100

    challenge

---
+++ 60-61 Consolidation V/Scope Creep Challenge/Scope Creep Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scope Creep Challenge
+//
+//  Created by Svidt on 25/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 60-61 Consolidation V/Scope Creep Challenge/Scope Creep Challenge/Scope_Creep_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scope_Creep_ChallengeApp.swift
+//  Scope Creep Challenge
+//
+//  Created by Svidt on 25/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Scope_Creep_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
