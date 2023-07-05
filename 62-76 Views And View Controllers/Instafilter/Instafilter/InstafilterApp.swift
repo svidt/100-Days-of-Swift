6f599277ba36b9628d8a51a0c922f892e07c132b
Emil <79602784+svidt@users.noreply.github.com>
2023-07-05 11:10:53 +0100

    new project

---
+++ 62-76 Views And View Controllers/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Kristian Emil Hansen Svidt on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
