7e1ba5245a40252d41d74c3547aaee488c6990da
Emil <79602784+svidt@users.noreply.github.com>
2023-07-10 22:56:44 +0100

    lesson 2

--- 62-76 Views And View Controllers/Instafilter/Instafilter/ContentView.swift
+++ 62-76 Views And View Controllers/Instafilter/Instafilter/ContentView.swift
@@ -5,28 +5,68 @@
 //  Created by Svidt on 05/07/2023.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0 {
-        didSet {
-            print("New value is \(blurAmount)")
-        }
-    }
+    @State private var image: Image?
     
+    @State private var blurAmount = 0.0
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.yellow
     
     var body: some View {
         VStack {
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
                 .blur(radius: blurAmount)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    showingConfirmation = true
+                }
+                .padding()
+                .border(backgroundColor, width: 5)
+                .cornerRadius(10)
+                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                    Button("Red") { backgroundColor = .red }
+                    Button("Green") { backgroundColor = .green }
+                    Button("blue") { backgroundColor = .blue }
+                    Button("Cancel", role: .cancel) { }
+                } message: {
+                    Text("Select a new color")
+                }
             
             Slider(value: $blurAmount, in: 0...20)
             
-            Button("Random Blur") {
+            Button("Random Amount") {
                 blurAmount = Double(.random(in: 0...20))
             }
+            
         }
-        .padding()
+        .onAppear(perform: loadImage)
+        .onChange(of: blurAmount) { newValue in
+            print("New value is \(newValue)")
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        currentFilter.radius = 1000
+        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
