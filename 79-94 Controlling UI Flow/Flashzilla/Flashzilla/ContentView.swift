83cf18827a7af762fcc2dd44165fbc501a936521
Emil <79602784+svidt@users.noreply.github.com>
2023-09-14 23:29:34 +0900

    timer functionality

--- 79-94 Controlling UI Flow/Flashzilla/Flashzilla/ContentView.swift
+++ 79-94 Controlling UI Flow/Flashzilla/Flashzilla/ContentView.swift
@@ -9,11 +9,25 @@ import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    
+
     @State private var engine: CHHapticEngine?
 
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+    
     var body: some View {
         VStack {
+            
+            Text("Hello!")
+                .onReceive(timer) { time in
+                    if counter == 5 {
+                        timer.upstream.connect().cancel()
+                    } else {
+                        print("The time is now \(time)")
+                    }
+                    counter += 1
+                }
+            
             HStack {
                 Circle()
                     .fill(.green)
