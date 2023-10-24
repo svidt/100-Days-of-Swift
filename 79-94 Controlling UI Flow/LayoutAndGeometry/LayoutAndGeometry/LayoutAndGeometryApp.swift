b2bce5b64e19f18221027ae9326ee9aa471fad63
Emil <79602784+svidt@users.noreply.github.com>
2023-10-24 19:30:43 +0900

    new project

---
+++ 79-94 Controlling UI Flow/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Kristian Emil Hansen Svidt on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
