6452d3d19f980c88eb9d5cbff9b1731d7c1e94ca
Emil <79602784+svidt@users.noreply.github.com>
2023-11-15 18:12:34 +0900

    back at it

--- 79-94 Controlling UI Flow/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 79-94 Controlling UI Flow/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -13,7 +13,15 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack(alignment: .firstTextBaseline) {
+                Text("Hello")
+                    .font(.largeTitle)
+                Text("Hello")
+                    .font(.title)
+                Text("Hello")
+                    .font(.title2)
+                
+            }
         }
         .padding()
     }
