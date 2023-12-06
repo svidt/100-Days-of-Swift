429bcbad7a25796a7d92f7ea7c89da8964cfbe32
Emil <79602784+svidt@users.noreply.github.com>
2023-12-06 09:12:03 +0100

    transfer

---
+++ 96-99 One Last Project/SnowSeeker/SnowSeeker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Kristian Emil on 06/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 96-99 One Last Project/SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Kristian Emil on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
